37d757b96d55b2e42c9b66354235638051d5e5fd
jmato9020 <mato9020@gmail.com>
2024-02-02 21:08:16 -0500

    app

---
+++ HomeNetDaemon.Desktop/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeNetDaemon.Desktop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
